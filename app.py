ff71d5e7bc117e9badd0e5f40d027698b2fc6430
Your Name <saiguptha_v@srmap.edu.in>
2025-03-16 21:21:25 +0530

    bindedn to all networks

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ app.register_blueprint(logBp, url_prefix="/api")
 
 if __name__ == '__main__':
     
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from the environment variable
+    app.run(host="0.0.0.0", port=port, debug=True)  # Bind to all network interfaces
